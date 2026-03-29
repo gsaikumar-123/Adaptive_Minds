77f685a7d597a30a69b7fd01a6d4e1f6079c9101
Siva Reddy <pavansivareddykummitha@gmail.com>
2026-03-29 13:35:42 +0530

    Expose forecast v2 progress route

--- backend/src/routes/progressRoutes.js
+++ backend/src/routes/progressRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { completeTopic, getCompletedTopics, getHeatmapData, uncompleteTopic } from "../controllers/progressController.js";
+import { completeTopic, getCompletedTopics, getHeatmapData, getLearningForecast, getLearningForecastV2, uncompleteTopic } from "../controllers/progressController.js";
 import { verifyToken } from "../middleware/verifyToken.js";
 
 const router = express.Router();
@@ -8,5 +8,7 @@ router.post("/complete", verifyToken, completeTopic);
 router.post("/uncomplete", verifyToken, uncompleteTopic);
 router.get("/completed", verifyToken, getCompletedTopics);
 router.get("/heatmap", verifyToken, getHeatmapData);
+router.get("/forecast", verifyToken, getLearningForecast);
+router.get("/forecast-v2", verifyToken, getLearningForecastV2);
 
 export default router;
